af9b33f1b491b63e986d89f26d020f113f724928
Pedro Soares <pr333do@gmail.com>
2026-09-10 20:31:02 -0300

    Initial Commit

---
+++ Sound Pad 3000/Sound_Pad_3000App.swift
@@ -0,0 +1,17 @@
+//
+//  Sound_Pad_3000App.swift
+//  Sound Pad 3000
+//
+//  Created by Pedro Soares on 10/09/26.
+//
+
+import SwiftUI
+
+@main
+struct Sound_Pad_3000App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
